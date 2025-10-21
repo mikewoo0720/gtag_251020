e8001a312426a2ec745c0a6f088d80e0bae2509a
mikewoo0720 <dnwhdgus93@gmail.com>
2025-10-21 10:07:14 +0900

    3

---
+++ script.js
@@ -0,0 +1,13 @@
+const handleSearch = () => {
+  const input = document.getElementById("searchInput").value.trim();
+
+  if (input !== "") {
+    gtag("event", "search_button_clicked", {
+      event_category: "engagement",
+      event_label: "search_keyword",
+      search_term: input,
+    });
+
+    alert(`검색어 ${input}에 대한 검색을 실행했습니다.`);
+  }
+};
